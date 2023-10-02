f2e6f130c2935a699c17ca4b529964cfa662b41d
Ibrahima Dieng <Dibou960@gmail.com>
2023-10-02 14:41:13 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GESTION_PEDA_IONIC',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
